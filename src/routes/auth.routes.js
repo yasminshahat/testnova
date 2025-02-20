95b433e9e916a911edd417cd2fb7c62e2ae4ccae
gazzaar <gazzarDev@gmail.com>
2025-02-20 18:48:57 +0200

    Implement auth contrller & Register and Login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,17 +1,14 @@
 const router = require('express').Router();
+const authController = require('../controllers/auth.controller');
 const authMiddleware = require('../middleware/auth.middleware');
 
 // Auth routes
-router.post('/login', (req, res) => {
-  // TODO: Implement login logic
-});
+router.post('/login', authController.login);
 
-router.post('/register', (req, res) => {
-  // TODO: Implement registration logic
-});
+router.post('/register', authController.register);
 
 router.post('/logout', authMiddleware, (req, res) => {
-  // TODO: Implement logout logic
+  res.json(successResponse(null, 'Logout successful'));
 });
 
 module.exports = router;
