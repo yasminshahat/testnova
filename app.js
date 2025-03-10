3fc23e3ed3e5928192a0fb922228c0f5a9a2a569
yasmin <yasminshahat61@gmail.com>
2025-03-10 02:21:45 +0200

    Edit app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/api',(req, res, next) => {
-    res.send('<h1>API</h1>');
+    res.send('<<h1>API</h1>>');
 });
 
 app.use('/',(req, res, next) => {
