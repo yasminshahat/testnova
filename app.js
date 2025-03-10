7e8a63c9e0d1be532cc48508c1d5dcc95d739af6
yasmin <yasminshahat61@gmail.com>
2025-03-10 02:09:51 +0200

    Edit app.js

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('in the middleware');
-    next();
+app.use('/api',(req, res, next) => {
+    res.send('<h1>API</h1>');
 });
 
-app.use((req, res, next) => {
-    console.log('in another middleware');
+app.use('/',(req, res, next) => {
+    res.send('<h1>hello from express</h1>');
 });
 
 app.listen(4000);
