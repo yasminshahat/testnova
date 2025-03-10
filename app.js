7548862749e9e68d5490f5eec0a278b49628a441
yasmin <yasminshahat61@gmail.com>
2025-03-10 02:03:11 +0200

    Edit app.js

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
+app.use((req, res, next) => {
+    console.log('in the middleware');
+    next();
+});
 
-server.listen(3000);
+app.use((req, res, next) => {
+    console.log('in another middleware');
+});
+
+app.listen(4000);
