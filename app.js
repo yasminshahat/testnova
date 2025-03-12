51170fe02ed3eb4c32127f9a01526607ce0f54a8
yasmin <yasminshahat61@gmail.com>
2025-03-12 02:34:43 +0200

    Add comments

--- app.js
+++ app.js
@@ -6,13 +6,19 @@ const authRoutes = require("./src/routes/auth.routes");
 const questionsRoutes = require("./src/routes/questions.routes");
 const examsRoutes = require("./src/routes/exams.routes");
 const resultsRoutes = require("./src/routes/results.routes");
+const exp = require('constants');
+
 const app = express();
+
+//Serve static files
+app.use(express.static(path.join(__dirname, '..public')));
 
 app.use(authRoutes);
 app.use(questionsRoutes);
 app.use(examsRoutes);
 app.use(resultsRoutes);
 
+//404 Page
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, '../public'))
 });
